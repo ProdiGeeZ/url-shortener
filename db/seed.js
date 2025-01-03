70685cce713cc28ef76b9e3f91bad0fa27339798
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2025-01-03 16:22:46 +0000

    Enhance seed script to create UUID extension and set default values for short_code and access_count in urls table

--- db/seed.js
+++ db/seed.js
@@ -5,14 +5,17 @@ const seed = ({ urlData }) => {
     return db
         .query(`DROP TABLE IF EXISTS urls;`)
         .then(() => {
+            return db.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+        })
+        .then(() => {
             return db.query(`CREATE TABLE urls (
-            id SERIAL PRIMARY KEY,
-            url TEXT NOT NULL UNIQUE,
-            short_code VARCHAR(10) UNIQUE,
-            descriptor VARCHAR(100),
-            access_count INTEGER,
-            created_at TIMESTAMP,
-            updated_at TIMESTAMP
+                id SERIAL PRIMARY KEY,
+                url TEXT NOT NULL UNIQUE,
+                short_code VARCHAR(10) UNIQUE DEFAULT SUBSTRING(uuid_generate_v4()::TEXT FROM 1 FOR 8),
+                descriptor VARCHAR(100),
+                access_count INTEGER DEFAULT 0 CHECK (access_count >= 0),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );`)
         })
         .then(() => {
