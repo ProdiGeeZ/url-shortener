27185685f2a3d994e1d51fa85aede06c44e3c6dc
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2025-01-06 23:35:39 +0000

    Implement GET /api/shorten/:shortcode endpoint to retrieve original URL and update API spec

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,4 +1,4 @@
-const { insertNewUrl } = require("../models/url.model");
+const { insertNewUrl, fetchOriginalUrl } = require("../models/url.model");
 const { validateURL } = require("../utils");
 
 exports.postNewUrl = (req, res, next) => {
@@ -9,5 +9,16 @@ exports.postNewUrl = (req, res, next) => {
     insertNewUrl(url, descriptor)
         .then((url) => {
             res.status(201).send({ url, msg: "Short URL created successfully" })
+        }).catch(next);
+}
+
+exports.getOriginalUrl = (req, res, next) => {
+    const { shortcode } = req.params;
+    fetchOriginalUrl(shortcode)
+        .then((url) => {
+            if (!url) {
+                return next({ status: 404, msg: "Not Found - Short URL does not exist" });
+            }
+            res.status(200).send({ url })
         }).catch(next);
 }
